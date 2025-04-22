241fc596b243831d1a4f22498d09579ddfc18492
Desiree Ferreira <desireeferreira6@gmail.com>
2025-04-22 10:58:31 -0300

    Atualização do app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListaCategorias from './components/Categoria/ListaCategorias';
 import DeletarCategoria from './components/Categoria/DeletarCategoria';
 import Home from './pages/Home';
 import FormularioCategoria from './components/Categoria/Formulario';
-import CardCategoria from './components/Categoria/CardCategoria';
+// import CardCategoria from './components/Categoria/CardCategoria';
 
 // import FormularioCategoria from './components/Categoria/FormularioCategoria';
 
@@ -21,9 +22,9 @@ function App() {
             <Route path="/home" element={<Home />} /> 
             <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
             <Route path="/FormularioCategoria" element={<FormularioCategoria />} />
-           {/* <Route path="/categorias" element={<ListaCategorias />} /> */}
+            <Route path="/listaprodutos" element={<ListaCategorias />} />
             <Route path="/cadastrarCategoria" element={<FormularioCategoria />} />
-            <Route path="/CardCategoria" element={<CardCategoria tema={undefined} />} />
+            {/* <Route path="/CardCategoria" element={<CardCategoria tema={undefined} />} /> */}
             
             
             </Routes>
